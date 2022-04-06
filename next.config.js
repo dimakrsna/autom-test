3531c9791d6be16ee4f2b30a7af211971ae9156a
Dima <dimakrsna@gmail.com>
2022-04-06 12:18:43 +0300

    Update config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const nextConfig = {
   reactStrictMode: false,
   i18n,
   env: {
-    EMAILJS_SERVICE_ID: 'service_boi1hki',
-    EMAILJS_TEMPLATE_ID_CONTACTS_FORM: 'template_n8s65n1',
-    EMAILJS_TEMPLATE_ID_DATASET_FORM: 'template_76zwj1r',
-    EMAILJS_USER_ID: '-ObtroasemqXb6HUV',
+    EMAILJS_SERVICE_ID: 'service_oih3105',
+    EMAILJS_TEMPLATE_ID_CONTACTS_FORM: 'template_sd7xewy',
+    EMAILJS_TEMPLATE_ID_DATASET_FORM: 'template_kuw78aj',
+    EMAILJS_USER_ID: '_UBOPUjYaWRlX5-br'
   },
 }
 
